93e79eab232aca4c90aaf1bc7f1bc96718ae8552
Christine Humphries <cmh@chrishumphries.net>
2019-09-05 21:55:41 -0400

    installed devise

--- Gemfile
+++ Gemfile
@@ -16,6 +16,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'bootstrap', '~> 4.3', '>= 4.3.1'
+gem 'jquery-rails'
+gem 'stock_quote', '~> 3.0'
+gem 'devise', '~> 4.7'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'pk_61fe698ab29349fe8bd0f1742be54f76')
+    
+    if params[:ticker] == ""
+      @nothing = "Hey! You forgot to enter a symbol."
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      #if stock varible doesn't exist, print error message
+      if !@stock
+        @error = "Hey! That stock symbol doesn't exist. Please try again."
+      end
+    end
+      
   end
+
+  def about
+  end
+
+
 end
